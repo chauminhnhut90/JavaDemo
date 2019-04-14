3973153c952cc36f9c419fa1a6c17489e3b50303
Nhut Chau <chauminhnhut90@gmail.com>
2019-04-14 20:37:49 +0700

    Lesson 2: Array, ArrayList, funtion

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,98 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        baitap1();
+        baitap2();
 
+        bai_1(10);
+        bai_2(10);
+        bai_3(10);
+    }
+
+    private static void baitap1() {
+
+        /*
+         * Tạo danh sách có 10 phần tử số nguyên (int) có giá trị từ 1 tới 10. Sau đó in ra
+         *
+         * Ghi chú: sử dụng cả Array và ArayList
+         * */
+
+        // Sử dụng Array
+        int[] a = new int[10];
+        for (int i = 0; i <= 9; i++) {
+            a[i] = i + 1;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+        // Sử dụng ArrayList
+        ArrayList<Integer> b = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            b.add(i);
+        }
+
+        for (int i = 0; i < b.size(); i++) {
+            System.out.println(b.get(i));
+        }
+    }
+
+    private static void baitap2() {
+        
         /**
-         *  Bài tập: Nhập số tuổi của 1 học sinh. In ra học sinh đó học cấp mấy.
-         */
+         * Bài tập 2:
+         *
+         * Tạo 1 mảng chứa tất cả các mùa trong năm. Sau đó in ra mảng giá trị
+         *
+         * Sử dụng Array or ArrayList
+         * */
 
-        int age = 18;
+        // Sử dụng Array
+        String[] a = new String[]{"XUAN", "HA", "THU", "DONG"};
 
-        // Code dung if ... else
-        if (age >= 6 && age <= 11) {
-            System.out.println("HOC SINH CAP 1");
-
-        } else if (age <= 15) {
-            System.out.println("HOC SINH CAP 3");
-
-        } else if (age <= 18) {
-            System.out.println("HOC SINH CAP 3");
-
-        } else if (age > 18) {
-            System.out.println("SINH VIEN DAI HOC");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
         }
 
-        // Code dung switch
-        switch (age) {
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-                break;
-
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("HOC SINH CAP 1");
-                break;
-
-            case 12:
-            case 13:
-            case 14:
-            case 15:
-                System.out.println("HOC SINH CAP 2");
-                break;
-
-            case 16:
-            case 17:
-            case 18:
-                System.out.println("HOC SINH CAP 3");
-                break;
-
-            default:
-                System.out.println("SINH VIEN DAI HOC");
-                break;
+        // Sử dụng ArrayList
+        ArrayList<String> b = new ArrayList<>();
+        b.add("XUAN");
+        b.add("HA");
+        b.add("THU");
+        b.add("DONG");
+        for (int i = 0; i < b.size(); i++) {
+            System.out.println(b.get(i));
         }
+    }
 
+
+    private static void bai_1(int n) {
+        // Tính S(n) = 1+2+3....+n;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("KQ bai_1: " + sum);
+    }
+
+    private static void bai_2(int n) {
+        // Tính S(n) = 1*2*3....*n;
+        int sum = 1;
+        for (int i = 1; i <= n; i++) {
+            sum *= i;
+        }
+        System.out.println("KQ bai_2: " + sum);
+    }
+
+    private static void bai_3(int n) {
+        // Tính S(n) = 1^2 + 2^2 + … + n^2
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + (i * i);
+        }
+        System.out.println("KQ bai_3: " + sum);
     }
 }
